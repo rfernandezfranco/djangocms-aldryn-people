403af8d14816d914150d6b00ac224d5904e77151
rfernandezfranco <rfernandezfranco@antel.com.uy>
2025-05-05 15:26:22 -0300

    Replace djangocms_text_ckeditor by djangocms_text.

--- aldryn_people/models.py
+++ aldryn_people/models.py
@@ -12,7 +12,7 @@ from cms.utils.i18n import get_current_language, get_default_language
 
 from aldryn_common.admin_fields.sortedm2m import SortedM2MModelField
 from aldryn_translation_tools.models import TranslatedAutoSlugifyMixin, TranslationHelperMixin
-from djangocms_text_ckeditor.fields import HTMLField
+from djangocms_text.fields import HTMLField
 from filer.fields.image import FilerImageField
 from parler.models import TranslatableModel, TranslatedFields
 
